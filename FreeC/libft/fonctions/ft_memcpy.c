5e6400a27e4677d29d6ed583eb20ced390ffbc35
Maya-Line PELAGE <mayaline.pelage@gmail.com>
2024-04-21 20:18:15 +0200

    ft_memcpy

---
+++ FreeC/libft/fonctions/ft_memcpy.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maya <maya@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/21 19:14:25 by maya              #+#    #+#             */
+/*   Updated: 2024/04/21 20:17:52 by maya             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void    *memcpy(void *restrict dst, const void  *restrict src, size_t n)
+{
+    size_t  i;
+    char	*ptr;
+    const char	*sptr;
+
+    ptr = (char *)dst;
+    sptr = (const char*)src;
+
+    i = 0;
+    while (i < n)
+    {
+        ptr[i] = sptr[i];
+        i++;
+    }
+    return (dst);
+}
+/*
+#include <stdio.h>
+#include <unistd.h>
+
+void *memcpy(void *restrict dst, const void *restrict src, size_t n);
+
+int main() {
+    char src[12] = "Hello, GPT!";
+    char dst[12] = {0};  // Destination array, uninitialized
+
+    printf("Before memcpy:\n");
+    printf("src : %s\n", src);  // Display the source array
+    printf("dst : %s\n", dst);  // This may print garbage or cause a crash due to uninitialized data
+
+    memcpy(dst, src, sizeof(src));
+
+    printf("After memcpy:\n");
+    printf("src : %s\n", src);  // Display the source array again
+    printf("dst : %s\n", dst);  // Now this should print "Hello, GPT!" just like src
+
+    return 0;
+}
+*/
