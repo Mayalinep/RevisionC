d278ee3052b23983faf2a02a8142019ef6e277bf
Maya-Line PELAGE <mayaline.pelage@gmail.com>
2024-04-21 15:02:55 +0200

    ft_bzero

---
+++ FreeC/libft/fonctions/ft_bzero.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maya <maya@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/19 23:00:23 by maya              #+#    #+#             */
+/*   Updated: 2024/04/21 15:01:23 by maya             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void    ft_bzero(void *s, size_t n)
+{
+    unsigned char	*str = (unsigned  char *)s;
+    size_t i;
+
+    i = 0;
+    while (i < n)
+    {
+        str[i] = 0;
+        i++;
+    }
+    
+}
+/*
+#include <stdio.h>
+#include <unistd.h>
+
+void    ft_bzero(void *s, size_t n);
+
+int main() {
+    char array[10]; // Déclaration d'un tableau de 10 caractères
+    int i = 0;      // Variable pour l'index, utilisée dans les boucles while
+
+    // Initialisation du tableau avec des valeurs non nulles
+    while (i < 10) {
+        array[i] = 'a' + i; // Assigner 'a', 'b', 'c', ..., 'j' au tableau
+        i++;
+    }
+
+    // Affichage du tableau avant l'appel à ft_bzero
+    printf("Before ft_bzero: ");
+    i = 0; // Réinitialiser l'index à 0 pour la nouvelle boucle
+    while (i < 10) {
+        printf("%c ", array[i]);
+        i++;
+    }
+    printf("\n");
+
+    // Appel de ft_bzero pour mettre à zéro tout le tableau
+    ft_bzero(array, 10);
+
+    // Affichage du tableau après l'appel à ft_bzero
+    printf("After ft_bzero:  ");
+    i = 0; // Réinitialiser l'index à 0 pour la nouvelle boucle
+    while (i < 10) {
+        printf("%d ", array[i]); // %d pour afficher les valeurs entières (attendu: 0)
+        i++;
+    }
+    printf("\n");
+
+    return 0;
+}
+*/
